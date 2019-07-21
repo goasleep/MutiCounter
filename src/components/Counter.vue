bf78206f1469ce9501e851bdef5b07dac1bba1db
goasleep <pfgoaleep@foxmail.com>
2019-07-21 10:30:56 +0800

    create a CounterGroup

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,1 +1,30 @@
-<
+<template>
+    <div>
+        <button>-</button>
+        <span>{{value}}</span>
+        <button>+</button>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            value: 0
+        }
+    },
+    methods: {
+        add() {
+           this.value++; 
+           this.$emit("counterValue",1)
+        },
+        delete(){
+           this.value--;
+           this.$emit("counterValue",-1)
+        }
+    },
+}
+</script>
+
---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Counter v-for="n in number" v-bind:key="n" @counterValue = "countSum"></Counter>
+    </div>
+</template>
+
+<script>
+import Couter from './components/Counter.vue';
+    export default {
+        name: "CounterGroup",
+        components: {
+            Counter,
+        },
+        props: {
+            number: {
+                type: Number,
+            },
+        },
+        data() {
+            return {
+                countSum: 0
+            }
+        },
+        methods: {
+            countSum(number) {
+                countSum += number
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
